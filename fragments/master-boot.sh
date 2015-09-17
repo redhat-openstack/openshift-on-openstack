1378b4a67bc622376eb53d866721c47cfc84e179
Tomas Sedovic <tomas@sedovic.cz>
2015-09-17 10:59:03 +0200

    Stop disabling NetworkManager
    It is not actually necessary (any more?).

--- fragments/master-boot.sh
+++ fragments/master-boot.sh
@@ -5,12 +5,6 @@ set -x
 set -o pipefail
 
 # master and nodes
-# Disable NetworkManager
-systemctl disable NetworkManager || true
-/sbin/chkconfig network on || true
-systemctl stop NetworkManager &>/dev/null || true
-
-# master and nodes
 # Set the DNS to the one provided
 sed -i 's/search openstacklocal/&\nnameserver $DNS_IP/' /etc/resolv.conf
 
