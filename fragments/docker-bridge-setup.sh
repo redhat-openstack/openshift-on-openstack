6aa1099ab69d896ab5f55eb916eaf73766d55e6e
Tomas Sedovic <tomas@sedovic.cz>
2015-10-07 20:07:56 +0200

    Experimental neutron setup using flannel
    This gives us pod to pod connectivity, but the pods seem to be unable to
    talk to the outside world or the management network.

--- fragments/docker-bridge-setup.sh
+++ fragments/docker-bridge-setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 retry yum -y install bridge-utils
 
-retry yum -y install \
-    http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+retry yum -y install https://kojipkgs.fedoraproject.org//packages/flannel/0.5.3/5.fc24/x86_64/flannel-0.5.3-5.fc24.x86_64.rpm
 
-retry yum install -y --enablerepo=epel jq
-
+cd /root
 
 cp /etc/origin/node/system:node:$(hostname).key etcd.key
 cp /etc/origin/node/system:node:$(hostname).crt etcd.crt
@@ -18,18 +15,14 @@ CA=/etc/origin/node/ca.crt
 CERT=$(pwd)/etcd.crt
 KEY=$(pwd)/etcd.key
 
-NETWORK=$(curl --cacert $CA --cert $CERT --key $KEY https://openshift-master.example.com:4001/v2/keys/openshift.io/registry/sdnnetworks/default | jq --raw-output .node.value | jq --raw-output .network)
-SUBNET=$(curl --cacert $CA --cert $CERT --key $KEY https://openshift-master.example.com:4001/v2/keys/openshift.io/registry/sdnsubnets/$(hostname) | jq --raw-output .node.value | jq --raw-output .subnet)
+curl -L --cacert $CA --cert $CERT --key $KEY https://openshift-master.example.com:4001/v2/keys/coreos.com/network/config -XPUT --data-urlencode value@flannel-config.json
 
-ip link set eth1 down # just in case
-brctl addbr br1
-ip addr add $NETWORK dev br1
-brctl addif br1 eth1
-ip link set eth1 up
-ip link set br1 up
+cp flannel-sysconfig /etc/sysconfig/flanneld
 
-ESCAPED_SUBNET=$(echo $SUBNET | sed -e 's/\//\\\//')
-sed -i -e "s/\$DOCKER_NETWORK_OPTIONS/-b=br1 --fixed-cidr=$ESCAPED_SUBNET/" /usr/lib/systemd/system/docker.service
+systemctl restart flanneld
 
-systemctl daemon-reload
+DOCKER_NET=$(ip a show docker0 | grep inet | awk '{print $2}')
+ip a del $DOCKER_NET dev docker0
 systemctl restart docker
+
+systemctl restart origin-node
