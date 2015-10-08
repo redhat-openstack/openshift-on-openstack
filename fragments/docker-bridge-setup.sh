d123d7a75904a756d926eee46c172764795e1c98
Tomas Sedovic <tomas@sedovic.cz>
2015-10-08 10:27:18 +0200

    Do flannel config only once

--- fragments/docker-bridge-setup.sh
+++ fragments/docker-bridge-setup.sh
@@ -6,18 +6,7 @@ retry yum -y install bridge-utils
 
 retry yum -y install https://kojipkgs.fedoraproject.org//packages/flannel/0.5.3/5.fc24/x86_64/flannel-0.5.3-5.fc24.x86_64.rpm
 
-cd /root
-
-cp /etc/origin/node/system:node:$(hostname).key etcd.key
-cp /etc/origin/node/system:node:$(hostname).crt etcd.crt
-
-CA=/etc/origin/node/ca.crt
-CERT=$(pwd)/etcd.crt
-KEY=$(pwd)/etcd.key
-
-curl -L --cacert $CA --cert $CERT --key $KEY https://openshift-master.example.com:4001/v2/keys/coreos.com/network/config -XPUT --data-urlencode value@flannel-config.json
-
-cp flannel-sysconfig /etc/sysconfig/flanneld
+cp /root/flannel-sysconfig /etc/sysconfig/flanneld
 
 systemctl restart flanneld
 
--- fragments/master-boot.sh
+++ fragments/master-boot.sh
@@ -43,6 +43,16 @@ systemctl restart iptables
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook --inventory /var/lib/ansible-inventory playbooks/byo/config.yml
 
+# Configure flannel
+cd /root
+cp /etc/origin/node/system:node:$(hostname).key etcd.key
+cp /etc/origin/node/system:node:$(hostname).crt etcd.crt
+CA=/etc/origin/node/ca.crt
+CERT=$(pwd)/etcd.crt
+KEY=$(pwd)/etcd.key
+curl -L --cacert $CA --cert $CERT --key $KEY https://openshift-master.example.com:4001/v2/keys/coreos.com/network/config -XPUT --data-urlencode value@flannel-config.json
+
+
 ansible -i /var/lib/ansible-inventory all -a 'docker-bridge-setup'
 
 echo "OpenShift has been installed."
