3325e57245f6d9eb8b2fdc6946c3de879db0afcb
Jan Provaznik <jprovazn@redhat.com>
2015-11-24 15:22:30 +0100

    Fix os-collect-config install on RHEL

--- collect-config-setup/fragments/install_config_agent_yum.sh
+++ collect-config-setup/fragments/install_config_agent_yum.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -eux
 
-yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-yum -y install https://repos.fedorapeople.org/repos/openstack/openstack-kilo/rdo-release-kilo-1.noarch.rpm
+if ! yum info os-collect-config; then
+    # if os-collect-config package is not available, first check if
+    # the repo is available but disabled, otherwise install the package
+    # from epel
+    if yum repolist disabled|grep rhel-7-server-openstack-7.0-director-rpms; then
+        subscription-manager repos --enable="rhel-7-server-openstack-7.0-director-rpms"
+        subscription-manager repos --enable="rhel-7-server-openstack-7.0-rpms"
+    else
+        # for now if os-collect-config is not available from any of existing
+        # repositories, add tripleo's default repo which contains these packages:
+        yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+        yum -y install https://repos.fedorapeople.org/repos/openstack/openstack-kilo/rdo-release-kilo-1.noarch.rpm
+    fi
+fi
 yum -y install os-collect-config python-zaqarclient os-refresh-config os-apply-config
 #yum-config-manager --disable 'epel*'
