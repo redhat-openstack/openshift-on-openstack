d5893ca8a0cb3c2b0e7a5c805c008c75aa0b59c9
Mark Lamourine <markllama@gmail.com>
2016-03-02 10:17:20 -0500

    refactored boot scripts for infra, master and node to use functions added commentary

--- fragments/infra-boot.sh
+++ fragments/infra-boot.sh
@@ -1,86 +1,258 @@
 #!/bin/bash
+#
+# Prepare the infrastructure server for Docker and Ansible
+#
+# ENVVARS
+#   WC_NOTIFY - a curl URL fragment from an OpenStack WaitCondition
+#               used to signal OpenStack of completion status
+#   DNS_IP    - The IP address of the nearest resolver host
+#
+#   SKIP_DNS  - true|false - enable local DNS service updates
+#
+#   OPENSHIFT_ANSIBLE_GIT_URL - the URL of a git repository containing the
+#                               openshift ansible playbooks and configs
+#   OPENSHIFT_ANSIBLE_GIT_REV - the release/revision of the playbooks to use
+#
 
+# Exit on first command failure or undefined var reference
 set -eu
 set -x
+
+# Return the non-zero exit code of the last cmd of a pipe (or 0 for success)
 set -o pipefail
 
+# CONSTANTS
+#
+# The device to mount to store Docker images and containers
+DOCKER_VOLUME_DEVICE=/dev/vdb
+
+# The auxiliary service container images - for Atomic hosts
+DNS_CONTAINER_IMAGE=jprovaznik/ooshift-dns
+HEAT_AGENT_CONTAINER_IMAGE=jprovaznik/ooshift-heat-agent
+
+# Select the EPEL release to make it easier to update
+EPEL_RELEASE_VERSION=7-5
+
+# Send success status to OpenStack WaitCondition
 function notify_success() {
-    $WC_NOTIFY --data-binary  "{\"status\": \"SUCCESS\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    $WC_NOTIFY --data-binary \
+               "{\"status\": \"SUCCESS\", \"reason\": \"$1\", \"data\": \"$1\"}"
     exit 0
 }
 
+# Send success status to OpenStack WaitCondition
 function notify_failure() {
-    $WC_NOTIFY --data-binary "{\"status\": \"FAILURE\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    $WC_NOTIFY --data-binary \
+               "{\"status\": \"FAILURE\", \"reason\": \"$1\", \"data\": \"$1\"}"
     exit 1
 }
 
-#[ -e /run/ostree-booted ] && notify_success "OpenShift node has been prepared for running ansible."
-#systemctl is-enabled os-collect-config || notify_failure "os-collect-config service is not installed or enabled"
-
-# master and nodes
-sed -i -e 's/^PEERDNS.*/PEERDNS="no"/' /etc/sysconfig/network-scripts/ifcfg-eth0
-
-if [ -e /run/ostree-booted ]; then
-    # Set the DNS to the one provided
+# 
+# --- DNS functions ----------------------------------------------------------
+#
+function save_resolver_file() {
     cp /etc/resolv.conf /etc/resolv.conf.local
-    # FIXME - the clean way would be to set nameserver for openstack's neutron network
-    [ "$SKIP_DNS" = "true" ] || sed -i 's/search openstacklocal.*/&\nnameserver $DNS_IP/' /etc/resolv.conf
+}
 
-    HOME=/root
-    mv /etc/systemd/system/multi-user.target.wants/docker-storage-setup.service $HOME
+# true if local DNS service is enabled
+function use_local_dns() {
+    [ "$SKIP_DNS" != "true" ]
+}
+
+# add the local nameserver to the beginning of the local resolver list
+function add_nameserver() {
+    # NAMESERVER_IP=$1
+    sed -i "/search openstacklocal.*/anameserver $1" /etc/resolv.conf
+}
+
+# Disable automatic updates of resolv.conf when an interface comes up
+function disable_resolv_updates() {
+    # INTERFACE=$1
+    sed -i -e '/^PEERDNS=/s/=.*/="no"/' \
+        /etc/sysconfig/network-scripts/ifcfg-$1
+}
+
+
+# ----------------------------------------------------------------------------
+# Functions for Atomic Host systems
+# ----------------------------------------------------------------------------
+
+# check if this is an Atomic host
+function is_atomic_host() {
+    [ -e /run/ostree-booted ]
+}
+
+# remove the docker storage setup service link and re-load the systemd config
+function systemd_docker_disable_storage_setup() {
+    mv /etc/systemd/system/multi-user.target.wants/docker-storage-setup.service /root
     systemctl daemon-reload
+}
 
-    # Setup Docker Storage Volume Group
-    if ! [ -b /dev/vdb ]; then
-      echo "ERROR: device /dev/vdb does not exist" >&2
-      notify_failure "device /dev/vdb does not exist"
+# All hosts must have an external disk device (cinder?) for docker storage
+function docker_check_for_storage_device() {
+    # DOCKER_VOLUME_DEVICE=$1
+    if [ ! -b $1 ]
+    then
+        notify_failure \
+            "docker volume device $1 does not exist"
     fi
+}
+
+# configure docker storage
+function docker_set_storage_device() {
+    # DOCKER_VOLUME_DEVICE=$1
+    cat << EOF > /etc/sysconfig/docker-storage-setup
+DEVS=$1
+VG=docker-vg
+EOF
+}
+
+
+#
+# --- OpenShift Auxiliary Service Containers
+#
+
+# The DNS container runs Dnsmasq from root file space
+function start_dns_container() {
+    # DNS_CONTAINER_IMAGE=$1
+    docker pull $1 ||
+        notify_failure "failed to pull dns docker image: $1"
+    docker run \
+           --name dnsmasq \
+           --detach \
+           --publish 53:53/udp \
+           --volume /var/log:/var/log \
+           --volume /etc/hosts:/etc/hosts \
+           --volume /etc/dnsmasq.conf:/etc/dnsmasq.conf \
+           --volume /etc/resolv.conf.local:/etc/resolv.conf \
+           $1 ||
+        notify_failure "failed to run dns docker image: $1"
+}
+
+function start_heat_agent_container() {
+    # HEAT_AGENT_CONTAINER_IMAGE=$1
+    docker pull $1 ||
+        notify_failure "failed to pull heat agent docker image: $1"
+    docker run \
+           --name heat-agent \
+           --detach \
+           --privileged \
+           --ipc=host \
+           --net=host \
+           --pid=host \
+           -e HOST=/host \
+           -e NAME=rhel-tools \
+           --volume /run:/run \
+           --volume /var/log:/var/log \
+           --volume /etc/localtime:/etc/localtime \
+           --volume ~/.ssh:/root/.ssh \
+           --volume /:/host \
+           --volume /etc/ansible:/etc/ansible \
+           --volume /var/lib/heat-cfntools:/var/lib/heat-cfntools \
+           $1 ||
+        notify_failure "failed to run heat-agent docker image: $1"
+}
+
+# ----------------------------------------------------------------------------
+# Functions for RPM based systems
+# ----------------------------------------------------------------------------
+
+function verify_os_collect_config_is_installed() {
+    systemctl is-enabled os-collect-config ||
+        notify_failure "os-collect-config service is not installed or enabled"
+}
+
+function install_enable_start_dnsmasq() {
+    yum install -y dnsmasq || notify_failure "can't install dnsmasq"
+    systemctl enable dnsmasq || notify_failure "can't enable dnsmasq"
+    systemctl restart dnsmasq || notify_failure "can't start dnsmasq"
+}
+
+function install_epel_repos_disabled() {
+    # EPEL_RELEASE=$1 - hyphen delimiter
+    # NOTE: install the right Ansible version on RHEL7.1 and Centos 7.1:
+    local EPEL_REPO_URL=http://dl.fedoraproject.org/pub/epel/7/x86_64
+    if ! rpm -q epel-release-$1
+    then
+        yum -y install \
+            ${EPEL_REPO_URL}/e/epel-release-$1.noarch.rpm ||
+            notify_failure "could not install EPEL release $1"
+    fi
+    sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+}
+
+#
+# Check out the Ansible playbooks from a Git repository
+#
+function clone_openshift_ansible() {
+    # GIT_URL=$1
+    # GIT_REV=$2
+    git clone "$1" /usr/share/ansible/openshift-ansible \
+            || notify_failure "could not clone openshift-ansible: $1"
+    cd /usr/share/ansible/openshift-ansible
+    git checkout "$2" ||
+        notify_failure "could not check out openshift-ansible rev $2"
+}
+
+# =============================================================================
+# MAIN
+# =============================================================================
+
+# Do not update resolv.conf from eth0 when the system boots
+disable_resolv_updates eth0
+
+if is_atomic_host
+then
+    # Prepare an Atomic host to act as the infrastructure server
+    save_resolver_file
+    use_local_dns && add_nameserver $DNS_IP
+
+    systemd_docker_disable_storage_setup
+
+    docker_check_for_storage_device $DOCKER_VOLUME_DEVICE
+    docker_set_storage_device $DOCKER_VOLUME_DEVICE
 
     systemctl enable lvm2-lvmetad
     systemctl start lvm2-lvmetad
-cat << EOF > /etc/sysconfig/docker-storage-setup
-DEVS=/dev/vdb
-VG=docker-vg
-EOF
-    /usr/bin/docker-storage-setup || notify_failure "failed to run docker-storage-setup"
-    systemctl start docker --ignore-dependencies || notify_failure "failed to start docker"
 
-    docker pull jprovaznik/ooshift-dns
-    docker run -d -p 53:53/udp -v /var/log:/var/log -v /etc/hosts:/etc/hosts -v /etc/dnsmasq.conf:/etc/dnsmasq.conf -v /etc/resolv.conf.local:/etc/resolv.conf --name dnsmasq jprovaznik/ooshift-dns || notify_failure "failed to run dns docker image"
-    docker pull jprovaznik/ooshift-heat-agent
-    docker run -d --privileged --ipc=host --net=host --pid=host -e HOST=/host -e NAME=rhel-tools -v /run:/run -v /var/log:/var/log -v /etc/localtime:/etc/localtime -v ~/.ssh:/root/.ssh -v /:/host -v /etc/ansible:/etc/ansible -v /var/lib/heat-cfntools:/var/lib/heat-cfntools --name heat-agent jprovaznik/ooshift-heat-agent || notify_failure "failed to run heat-agent docker image"
+    docker-storage-setup || notify_failure "docker storage setup failed"
+
+    systemctl start docker --ignore-dependencies ||
+        notify_failure "docker service failed to start"
+
+    start_dns_container $DNS_CONTAINER_IMAGE
+    start_heat_agent_container $HEAT_AGENT_CONTAINER_IMAGE
+    
 else
-    systemctl is-enabled os-collect-config || notify_failure "os-collect-config service is not installed or enabled"
-    if [ "$SKIP_DNS" != "true" ]; then
-        yum install -y dnsmasq || notify_failure "can't install dnsmasq"
-        systemctl enable dnsmasq || notify_failure "can't enable dnsmasq"
-        systemctl restart dnsmasq || notify_failure "can't start dnsmasq"
-    fi
+    # Prepare a RHEL host to act as the infrastructure server
+    verify_os_collect_config_is_installed
 
-    yum install -y git httpd-tools || notify_failure "could not install httpd-tools"
+    use_local_dns && install_enable_start_dnsmasq
 
-    # for centos openssl pkg is not included in pkg requirements yet,
-    # make sure it's present
-    yum -y install pyOpenSSL || notify_failure "could not install pyOpenSSL"
+    # Install the EPEL repository, but leave it disabled
+    # Used only to install Ansible
+    install_epel_repos_disabled $EPEL_RELEASE_VERSION
 
-    # NOTE: install the right Ansible version on RHEL7.1 and Centos 7.1:
-    if ! rpm -q epel-release-7-5;then
-        yum -y install \
-            http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm \
-            || notify_failure "could not install EPEL"
-    fi
-    sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
-    yum -y --enablerepo=epel install ansible || notify_failure "could not install ansible"
+    yum -y install git httpd-tools ||
+        notify_failure "could not install httpd-tools"
+    
+    # ensure openssl is installed on CentOS
+    yum -y install pyOpenSSL ||
+        notify_failure "could not install pyOpenSSL"
 
-    if [ -n "$OPENSHIFT_ANSIBLE_GIT_URL" ] && [ -n "$OPENSHIFT_ANSIBLE_GIT_REV" ]
+    # Install from the EPEL repository
+    yum -y --enablerepo=epel install ansible ||
+        notify_failure "could not install ansible"
+        
+    if [ -n "$OPENSHIFT_ANSIBLE_GIT_URL" -a -n "$OPENSHIFT_ANSIBLE_GIT_REV" ]
     then
-        git clone "$OPENSHIFT_ANSIBLE_GIT_URL" /usr/share/ansible/openshift-ansible \
-            || notify_failure "could not clone openshift-ansible"
-        cd /usr/share/ansible/openshift-ansible
-        git checkout "$OPENSHIFT_ANSIBLE_GIT_REV"
+        clone_openshift_ansible \
+            $OPENSHIFT_ANSIBLE_GIT_URL \
+            $OPENSHIFT_ANSIBLE_GIT_REV
     else
         yum -y install openshift-ansible-roles openshift-ansible-playbooks \
             || notify_failure "could not install openshift-ansible"
     fi
 fi
+
 notify_success "OpenShift node has been prepared for running ansible."
--- fragments/master-boot.sh
+++ fragments/master-boot.sh
@@ -1,37 +1,142 @@
 #!/bin/bash
+#
+# Prepare an OpenShift node VM for configuration by Ansible
+#
+# ENVVARS
+#   WC_NOTIFY - a curl URL from an OpenStack WaitCondition
+#               send status to OpenStack
+#   SKIP_DNS - local DNS is disabled: do not try to make updates
+#
+# CONSTANTS
+#
+# The device to mount to store Docker images and containers
+DOCKER_VOLUME_DEVICE=/dev/vdb
 
+# Exit on first fail or on reference to an undefined variable
 set -eu
 set -x
+
+# Return the exit code of the last non-zero command in a pipe (or 0 on success)
 set -o pipefail
 
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+# - WaitConditions ---------------------------------------------------------
+# Send success status to OpenStack via curl URL
 function notify_success() {
-    $WC_NOTIFY --data-binary  "{\"status\": \"SUCCESS\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    $WC_NOTIFY --data-binary  \
+               "{\"status\": \"SUCCESS\", \"reason\": \"$1\", \"data\": \"$1\"}"
     exit 0
 }
 
+# Send fail status to OpenStack via curl URL
 function notify_failure() {
-    $WC_NOTIFY --data-binary "{\"status\": \"FAILURE\", \"reason\": \"$1\", \"data\": \"$1\"}"
+    $WC_NOTIFY --data-binary \
+               "{\"status\": \"FAILURE\", \"reason\": \"$1\", \"data\": \"$1\"}"
     exit 1
 }
 
-# master and nodes
-# Set the DNS to the one provided
-[ "$SKIP_DNS" = "true" ] || sed -i 's/search openstacklocal.*/&\nnameserver $DNS_IP/' /etc/resolv.conf
-sed -i -e 's/^PEERDNS.*/PEERDNS="no"/' /etc/sysconfig/network-scripts/ifcfg-eth0
+
+#
+# - DNS ---------------------------------------------------------
+#
+
+# boolean: true if local DNS service is enabled
+function dns_updates_enabled() {
+    [ "$SKIP_DNS" != "true" ]
+}
+
+
+# add the local nameserver to the beginning of the local resolver list
+function add_nameserver() {
+    # NAMESERVER_IP=$1
+    sed -i "/search openstacklocal.*/anameserver $1" /etc/resolv.conf
+}
+
+function disable_peerdns() {
+    # INTERFACE=$1
+    sed -i '/^PEERDNS=/s/=.*/="no"/' /etc/sysconfig/network-scripts/ifcfg-$1
+}
+
+#
+# - sudo ---------------------------------------------------------
+#
+
+function sudo_enable_from_ssh() {
+    # Required for SSH pipelining
+    sed -i "/requiretty/s/^/#/" /etc/sudoers
+}
 
 # workaround for openshift-ansible - symlinks are created in /usr/local/bin but
 # this path is not by default in sudo secure_path so ansible fails
-sed -i 's,secure_path = /sbin:/bin:/usr/sbin:/usr/bin,secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin,' /etc/sudoers
+function sudo_set_secure_path() {
+    # SECURE_PATH=$1
+    sed -i "/secure_path = /s|=.*|= $1|" /etc/sudoers
+}
 
-rpm -q docker || yum -y install docker || notify_failure "could not install docker"
-echo "INSECURE_REGISTRY='--insecure-registry 0.0.0.0/0'" >> /etc/sysconfig/docker
-systemctl enable docker
+#
+# - docker ---------------------------------------------------------
+#
 
-# Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1289851
-# can be removed once docker 1.10 is released
-cp /lib/systemd/system/docker.service /etc/systemd/system/docker.service
-sed -i 's/Wants=docker-storage-setup.service/&\nRequires=docker.socket/' /etc/systemd/system/docker.service
-cat << EOF > /etc/systemd/system/docker.socket
+function docker_install_and_enable() {
+    if ! rpm -q docker
+    then
+        yum -y install docker || notify_failure "could not install docker"
+    fi
+    systemctl enable docker
+}
+
+
+# 
+function docker_version() {
+    # MAJ_MIN=$1 - 'major' or 'minor'
+    local version=$(rpm -q docker --qf '%{VERSION}')
+    [ $1 = "major" ] && echo $version | cut -d. -f1 && return
+    [ $1 = "minor" ] && echo $version | cut -d. -f2 && return
+    echo $version
+}
+
+function docker_set_trusted_registry() {
+    # TRUSTED_REGISTRY=$1
+    echo "INSECURE_REGISTRY='--insecure-registry $1'" >> /etc/sysconfig/docker
+}
+
+# All hosts must have an external disk device (cinder?) for docker storage
+function docker_check_for_storage_device() {
+    # DOCKER_VOLUME_DEVICE=$1
+    if ! [ -b $1 ]; then
+        notify_failure \
+            "docker volume device $1 does not exist"
+    fi
+}
+
+function docker_set_storage_device() {
+    # DOCKER_VOLUME_DEVICE=$1
+    cat << EOF > /etc/sysconfig/docker-storage-setup
+DEVS=$1
+VG=docker-vg
+EOF
+}
+
+#
+# - systemd ---------------------------------------------------------
+#
+
+function systemd_add_docker_socket() {
+    
+    # Workaround for https://bugzilla.redhat.com/show_bug.cgi?id=1289851
+    # can be removed once docker 1.10 is released
+    
+    # make a "local" copy of the docker service unit file
+    cp /lib/systemd/system/docker.service /etc/systemd/system/docker.service
+    # Add a new unit file requirement
+    sed -i '/Wants=docker-storage-setup.service/aRequires=docker.socket' \
+        /etc/systemd/system/docker.service
+
+    # create the docker socket unit file
+    cat << EOF > /etc/systemd/system/docker.socket
 [Unit]
 Description=Docker Socket for the API
 PartOf=docker.service
@@ -45,24 +150,44 @@ SocketGroup=root
 [Install]
 WantedBy=sockets.target
 EOF
-systemctl daemon-reload
 
-# Required for SSH pipelining
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+    # Force re-read of systemd configuration and apply
+    systemctl daemon-reload
+}
 
-# Setup Docker Storage Volume Group
-if ! [ -b /dev/vdb ]; then
-  echo "ERROR: device /dev/vdb does not exist" >&2
-  notify_failure "device /dev/vdb does not exist"
+
+############################################################################
+# MAIN
+############################################################################
+
+if dns_updates_enabled
+then
+    add_nameserver $DNS_IP
 fi
 
+disable_peerdns eth0
+
+sudo_set_secure_path "/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin"
+sudo_enable_from_ssh
+
+docker_install_and_enable
+docker_set_trusted_registry 0.0.0.0/0
+
+if [ $(docker_version major) -lt 2 -a $(docker_version minor) -lt 10 ]
+then
+    systemd_add_docker_socket
+fi
+
+docker_check_for_storage_device $DOCKER_VOLUME_DEVICE
+
+# configure the external docker volume for LVM management
+docker_set_storage_device $DOCKER_VOLUME_DEVICE
+
+# lvmetad allows new volumes to be configured and made available as they appear
+# This is good for dynamically created volumes in a cloud provider service
 systemctl enable lvm2-lvmetad
 systemctl start lvm2-lvmetad
-cat << EOF > /etc/sysconfig/docker-storage-setup
-DEVS=/dev/vdb
-VG=docker-vg
-EOF
 
-/usr/bin/docker-storage-setup
+/usr/bin/docker-storage-setup || notify_failure "Docker Storage setup failed"
 
-notify_success "OpenShift node has been prepared for running ansible."
+notify_success "OpenShift node has been prepared for running docker."
