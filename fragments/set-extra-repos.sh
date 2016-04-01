c4eb17f581eef2ccafaa7e90f7acaba2c7d1a526
Mark Lamourine <markllama@gmail.com>
2016-04-01 10:12:37 -0400

    added comments and clarified the code.  Added original name to the repo name

--- fragments/set-extra-repos.sh
+++ fragments/set-extra-repos.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+#
+# Download and add a set of additional YUM repo files
+#   Apply a sum to each filename so that repo files from different sources but
+#   with the same name (ie "testing.repo") will not overwrite each other.
+#
+# ENVVARS
+#
+#  REPOLIST - a whitespace delimited list of URLs for YUM repo files.
+#
+# The target location for downloaded repo files.  Default to system location
+REPODIR=${REPODIR:-"/etc/yum.repos.d"}
 
 set -eux
 set -o pipefail
 
+# Bypass this function when no additional repos are provided.
 [ -z "$REPOLIST" ] && exit 0
-cd /etc/yum.repos.d
-for repo in $REPOLIST;do
-    fname=`echo "$repo"|md5sum |awk '{ print $1}'`.repo
-    curl -o $fname $repo
+
+#
+# Pull down each repo file from the URL, renaming it to avoid filename overload
+#
+for repofile_url in ${REPOLIST}
+do
+    # Create a path from the URL file and a hash to avoid conflict
+    url_filename=$(basename $repofile_url)
+    url_checksum=$(echo "$repofile_url" | md5sum | cut -f1 -d' ')
+    curl -o ${REPODIR}/${url_filename}-${url_checksum}.repo $repofile_url
 done
