754696a5ee1c6d27c420d2b1d695a0367ca980b1
Mark Lamourine <markllama@gmail.com>
2017-01-19 15:13:31 -0500

    Allow the user to select the OSP version on which the stack will run This effects the repos used to get the os-collect-config packages.
    add parameters to install script
    
    added osp_version to install config agent yaml
    
    add cascade of osp_version
    
    remove director RPMs repo
    
    update location of os-collect-config files

--- collect-config-setup/fragments/install_config_agent_yum.sh
+++ collect-config-setup/fragments/install_config_agent_yum.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eux
 
+# OSP_VERSION is set by ENV or by heat string replacement
+[ -n "$OSP_VERSION" ] || (echo "Missing required value OSP_VERSION" ; exit 1)
+
 # on Atomic host os-collect-config runs inside a container which is
 # fetched&started in another step
 [ -e /run/ostree-booted ] && exit 0
@@ -9,9 +12,11 @@ if ! yum info os-collect-config; then
     # if os-collect-config package is not available, first check if
     # the repo is available but disabled, otherwise install the package
     # from epel
-    if yum repolist disabled|grep rhel-7-server-openstack-8-director-rpms; then
-        subscription-manager repos --enable="rhel-7-server-openstack-8-director-rpms"
-        subscription-manager repos --enable="rhel-7-server-openstack-8-rpms"
+    if yum repolist disabled|grep rhel-7-server-openstack-${OSP_VERSION}-rpms; then
+        subscription-manager repos --enable="rhel-7-server-openstack-${OSP_VERSION}-rpms"
+        if [ "$OSP_VERSION" -lt 10 ] ; then
+            subscription-manager repos --enable="rhel-7-server-openstack-${OSP_VERSION}-director-rpms"
+        fi
     else
         yum -y install centos-release-openstack-liberty
     fi
